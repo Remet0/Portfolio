5fa8877574fb97d0fe595f4276397955e6404a76
Remeto <remetojohn@gmail.com>
2019-07-26 15:37:54 -0700

    added contact page, changed around some positioning and animation values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,35 @@
-function showImages(el){
-    var windowHeight = jQuery(window).height();
-    $(el).each(function(){
-        var thisPos = $(this).offset().top;
-//add var for div height to subtract from windowHieght
-//add if statement to remove fadeIn when outside(might need another calss Fadeout, remove one add other)
-        var topOfWindow = $(window).scrollTop();
-        if(topOfWindow + windowHeight - 300 > thisPos){
-            $(this).addClass("fadeIn");
-            $(this).addClass("fix");
-            $(this).removeClass("fadeOut")
-            $()
-        }else{
-            $(this).removeClass("fadeIn");
-            $(this).removeClass('fix');
-            $(this).addClass("fadeOut");
-        }
-})
+function showImages(el) {
+  var windowHeight = jQuery(window).height();
+  $(el).each(function() {
+    var thisPos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+    if (el === ".card--portfolio") {
+      if (
+        topOfWindow + windowHeight - 550 > thisPos &&
+        topOfWindow + windowHeight - 900 < thisPos
+      ) {
+        $(this).addClass("clip--Portfolio_In");
+        $(this).removeClass("clip--Portfolio_Out");
+      } else {
+        $(this).removeClass("clip--Portfolio_In");
+        $(this).addClass("clip--Portfolio_Out");
+      }
+  }else if(el === '.card--work'){
+    if (
+        topOfWindow + windowHeight - 200 > thisPos &&
+        topOfWindow + windowHeight - 900 < thisPos
+      ) {
+        $(this).addClass("clip--Work_In");
+        $(this).removeClass("clip--Work_Out");
+      } else {
+        $(this).removeClass("clip--Work_In");
+        $(this).addClass("clip--Work_Out");
+      }
+  }
+});
 }
-$(document).ready(function(){
-    showImages('#landing')
-
-})
-
-$(document).scroll(function(){
-    showImages('.card--portfolio')
-    showImages('.card--work');
-})
+$(document).scroll(function() {
+  showImages(".card--portfolio");
+  showImages(".card--work");
+});
